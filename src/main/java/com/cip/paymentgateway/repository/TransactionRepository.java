a48402fcdef0ae56e35d5d2914732946669b042d
Gerald Simanullang <gsimanullang@students.hacktiv8.ac.id>
2026-05-22 10:22:16 +0700

    feat: add transaction repository with custom query methods

---
+++ src/main/java/com/cip/paymentgateway/repository/TransactionRepository.java
@@ -0,0 +1,16 @@
+package com.cip.paymentgateway.repository;
+
+import com.cip.paymentgateway.entity.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface TransactionRepository extends JpaRepository<Transaction, UUID> {
+
+    Optional<Transaction> findByOrderId(String orderId);
+
+    boolean existsByOrderId(String orderId);
+}
